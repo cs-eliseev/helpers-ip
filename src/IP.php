da82ff8691f085d6bc76a2d1fcbfdf607caaecc5
cs-eliseev <a.k.eliseev@gmail.com>
2019-02-16 09:05:48 +0300

    add method get real IP address

---
+++ src/IP.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace cse\helpers;
+
+/**
+ * Class IP
+ *
+ * @package cse\helpers
+ */
+class IP
+{
+    /**
+     * Get real IP
+     *
+     * @return null|string
+     */
+    public static function getRealIP(): ?string
+    {
+        if (!empty($_SERVER['HTTP_X_REAL_IP'])) {
+            // Check ip from share internet
+            $ip = $_SERVER['HTTP_X_REAL_IP'];
+        } elseif (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            // Check ip from share internet
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            // To check ip is pass from proxy
+            $ips = explode(',', str_replace(' ', '', $_SERVER['HTTP_X_FORWARDED_FOR']));
+            $ips = array_filter($ips);
+            if (count($ips)) $ip = array_pop($ips);
+        }
+
+        return empty($ip) ? ($_SERVER['REMOTE_ADDR'] ?? null) : $ip;
+    }
+}
