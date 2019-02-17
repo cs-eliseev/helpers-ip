68c5e962358ee43c1d07c1eaa890a091aaf83e95
cs-eliseev <a.k.eliseev@gmail.com>
2019-02-17 09:00:23 +0300

    add example remove subnet mask IPv6

--- examples/examples-ip.php
+++ examples/examples-ip.php
@@ -4,7 +4,7 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'autol
 
 use cse\helpers\IP;
 
-// Example: get real ip address
+// Example: get real IP address
 // 10.10.10.162
 $_SERVER = [
     'HTTP_X_REAL_IP' => '',
@@ -12,4 +12,10 @@ $_SERVER = [
     'HTTP_X_FORWARDED_FOR' => '10.10.10.160, 10.10.10.161, 10.10.10.162',
 ];
 var_dump(IP::getRealIP());
+echo PHP_EOL;
+
+// Example: remove subnet mask IPv6
+// 2a0a:2b40::4:60
+var_dump(IP::removeSubnetMaskIPv6('2a0a:2b40::4:60'));
+var_dump(IP::removeSubnetMaskIPv6('2a0a:2b40::4:60/124'));
 echo PHP_EOL;
