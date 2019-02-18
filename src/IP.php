b5fe092a1ec204f7830e9786a4dfc5a98337bb41
cs-eliseev <a.k.eliseev@gmail.com>
2019-02-18 08:20:17 +0300

    add method get range IPv6 address

--- src/IP.php
+++ src/IP.php
@@ -91,4 +91,55 @@ class IP
     {
         return is_int(self::getVersionIP($ip));
     }
+
+    /**
+     * Get range IPv6 address
+     *
+     * @param $ip - ex. 2a0a:2b40::4:60/124
+     * @return array  - ex. {2a0a:2b40::4:60, 2a0a:2b40::4:6f}
+     */
+    public static function getRangeIPv6(string $ip): array
+    {
+        list($first_addr_str, $prefix_len) = explode('/', $ip);
+
+        // Parse the address into a binary string
+        $first_addr_bin = inet_pton($first_addr_str);
+        // Convert the binary string to a string with hexadecimal characters
+        $addr_hex = unpack('H*', $first_addr_bin);
+        $first_addr_hex = reset($addr_hex);
+        // Overwriting first address string to make sure notation is optimal
+        $first_addr_str = inet_ntop($first_addr_bin);
+        // Calculate the number of 'flexible' bits
+        $flex_bits = 128 - $prefix_len;
+        // Build the hexadecimal string of the last address
+        $last_addr_hex = $first_addr_hex;
+
+        // We start at the end of the string (which is always 32 characters long)
+        $pos = 31;
+        while ($flex_bits > 0) {
+            // Get the character at this position
+            $orig = substr($last_addr_hex, $pos, 1);
+            // Convert it to an integer
+            $origval = hexdec($orig);
+            // OR it with (2^flexbits)-1, with flexbits limited to 4 at a time
+            $new_val = $origval | (pow(2, min(4, $flex_bits)) - 1);
+            // Convert it back to a hexadecimal character
+            $new = dechex($new_val);
+            // And put that character back in the string
+            $last_addr_hex = substr_replace($last_addr_hex, $new, $pos, 1);
+            // We processed one nibble, move to previous position
+            $flex_bits -= 4;
+            $pos -= 1;
+        }
+
+        // Convert the hexadecimal string to a binary string
+        $last_addr_bin = pack('H*', $last_addr_hex);
+        // And create an IPv6 address from the binary string
+        $last_addr_str = inet_ntop($last_addr_bin);
+
+        return [
+            $first_addr_str,
+            $last_addr_str,
+        ];
+    }
 }
